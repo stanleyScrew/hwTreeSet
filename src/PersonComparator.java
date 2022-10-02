9653c7545c831d13646ad54080de38bfb503cbfe
stanleyScrew <igor.grishchenko74@gmail.com>
2022-10-02 22:12:00 +0300

    Changes 290922_1

--- src/PersonComparator.java
+++ src/PersonComparator.java
@@ -9,10 +9,10 @@ public class PersonComparator implements Comparator<Person> {
 
     @Override
     public int compare(Person p1, Person p2) {
-        int len1 = p1.getSurname().split(" ").length;
-        int len2 = p2.getSurname().split(" ").length;
-        if (Math.min(len1, maxLen) != Math.min(len2, maxLen)) {
-            return Integer.compare(len1, len2);
+        String[] len1 = p1.getSurname().split(" ");
+        String[] len2 = p2.getSurname().split(" ");
+        if (Math.min(len1.length, maxLen) != Math.min(len2.length, maxLen)) {
+            return Integer.compare(len1.length, len2.length);
         }
         return Integer.compare(p1.getAge(), p2.getAge());
 
